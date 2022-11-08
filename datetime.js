3906d0359300584f6b051670f37d4c3a19ec5299
mrepol742 <mrepol742@gmail.com>
2022-11-08 09:35:12 +0800

    Initial Commit

--- datetime.js
+++ datetime.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Online'));
 
