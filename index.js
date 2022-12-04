2d46d7063a06869dd985e71f957ced03121e86c1
mrepol742 <mrepol742@gmail.com>
2022-12-04 14:11:14 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1214,11 +1214,12 @@ async function ai(api, event) {
                     } else {
                         const {
                             data 
-                        } = await openai.createEdit("code-davinci-edit-001", {
-                            input:  text,
-                            instruction: "",
-                            temperature: 0,
-                            top_p: 1,
+                        } = await openai.createCompletion("code-davinci-002", {
+                            prompt: text,
+                            temperature: 0.0,
+                            max_tokens: 1,
+                            top_p: 0,
+                            longprobs: 0,
                         });
                         let data1 = data.choices[0].text;
                         saveAns.push([text, data1])
