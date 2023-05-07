4866c036a37cd3a3bf23f459d92642e1a5666513
Melvin Jones Repol <mrepol742@gmail.com>
2023-05-07 15:21:28 +0800

    Initial Commit

--- test5.js
+++ test5.js
@@ -1,39 +1,18 @@
-const path = require("path");
-const fs = require("fs/promises");
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
 
-const dirSize = async (dir) => {
-    const files = await fs.readdir(dir, { withFileTypes: true });
+db.serialize(() => {
+    db.run("CREATE TABLE lorem (info TEXT)");
 
-    const paths = files.map(async (file) => {
-        const path1 = path.join(dir, file.name);
+    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+    for (let i = 0; i < 10; i++) {
+        stmt.run("Ipsum " + i);
+    }
+    stmt.finalize();
 
-        if (file.isDirectory()) return await dirSize(path1);
-
-        if (file.isFile()) {
-            const { size } = await fs.stat(path1);
-
-            return size;
-        }
-
-        return 0;
+    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
+        console.log(row.id + ": " + row.info);
     });
+});
 
-    return (await Promise.all(paths)).flat(Infinity).reduce((i, size) => i + size, 0);
-};
-async function a() {
-    const size = await dirSize(__dirname + "/");
-    console.log(convertBytes(size));
-}
-a();
-let sizesM = ["Bytes", "KB", "MB", "GB", "TB"];
-
-const convertBytes = function (bytes) {
-    if (bytes == 0) {
-        return "n/a";
-    }
-    let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    if (i == 0) {
-        return bytes + " " + sizesM[i];
-    }
-    return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizesM[i];
-};
+db.close();
