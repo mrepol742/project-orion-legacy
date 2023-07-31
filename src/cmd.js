56842973f6a678e8ed7774ad6e8529e9115e6b09
Melvin Jones Repol <mrepol742@gmail.com>
2023-07-31 23:33:28 +0800

    Initial Commit

--- src/cmd.js
+++ src/cmd.js
@@ -22,361 +22,361 @@
 "use strict";
 
 let help = `
-Hello %USER%, how are you?
-
-   ⦿ func 
-       next, all, user, group,
-       admin and root
-   ⦿ stats
-   ⦿ uptime
-   ⦿ sysinfo
-   ⦿ tokens
-   ⦿ sendReport [text]
-   ⦿ search [text]
-   ⦿ searchincog [text]
-   ⦿ searchimg [text]
-   ⦿ searchimg --reverse
-   ⦿ dell [prompt]
-   ⦿ poli [prompt]
-   ⦿ run [lang] [reply]
-       Java, Python, C, C++,
-       JavaScript, PHP and Dragon
-   ⦿ mj [prompt]
-   ⦿ beshy [prompt]
-   ⦿ melbin [prompt]
-   ⦿ sim [prompt]
-   ⦿ misaka [prompt]
-   ⦿ codex [prompt]
-   ⦿ openai [prompt]
-   ⦿ chatgpt [prompt]
-   ⦿ chad [prompt]
-   ⦿ nraf [prompt]
-   ⦿ skynet [prompt]
-
-If you have further questions, i am here to help you.
+⋆｡° Hello %USER%, how are you?
+⏐
+⏐   ⦿ func 
+⏐       next, all, user, group,
+⏐       admin and root
+⏐   ⦿ stats
+⏐   ⦿ uptime
+⏐   ⦿ sysinfo
+⏐   ⦿ tokens
+⏐   ⦿ sendReport ⎾text⏌
+⏐   ⦿ search ⎾text⏌
+⏐   ⦿ searchincog ⎾text⏌
+⏐   ⦿ searchimg ⎾text⏌
+⏐   ⦿ searchimg --reverse
+⏐   ⦿ dell ⎾prompt⏌
+⏐   ⦿ poli ⎾prompt⏌
+⏐   ⦿ run ⎾lang⏌ ⎾reply⏌
+⏐       Java, Python, C, C++,
+⏐       JavaScript, PHP and Dragon
+⏐   ⦿ mj ⎾prompt⏌
+⏐   ⦿ beshy ⎾prompt⏌
+⏐   ⦿ melbin ⎾prompt⏌
+⏐   ⦿ sim ⎾prompt⏌
+⏐   ⦿ misaka ⎾prompt⏌
+⏐   ⦿ codex ⎾prompt⏌
+⏐   ⦿ openai ⎾prompt⏌
+⏐   ⦿ chatgpt ⎾prompt⏌
+⏐   ⦿ chad ⎾prompt⏌
+⏐   ⦿ nraf ⎾prompt⏌
+⏐   ⦿ skynet ⎾prompt⏌
+⏐
+⎩⎯  If you have further questions, i am here to help you.
 `;
 
 let help1 = `
-Hello %USER%, how can i assist you?
-
-   ⦿ showerthoughts
-   ⦿ lulcat [text]
-   ⦿ gemoji [emoji]
-   ⦿ translate [language] [reply]
-   ⦿ wiki [text]
-   ⦿ urlshort [url]
-   ⦿ pickup
-   ⦿ landscape
-   ⦿ landscape [text]
-   ⦿ portrait
-   ⦿ portrait [text]
-   ⦿ mal [text]
-   ⦿ mdl [text]
-   ⦿ pin add
-   ⦿ pin remove
-   ⦿ sadcat [text]
-   ⦿ biden [text]
-   ⦿ pika [text]
-   ⦿ god [text]
-   ⦿ qrcode [text]
-
-Some of my commands works in any means you could imagine.
+⋆｡° Hello %USER%, how can i assist you?
+⏐
+⏐   ⦿ showerthoughts
+⏐   ⦿ lulcat ⎾text⏌
+⏐   ⦿ gemoji ⎾emoji⏌
+⏐   ⦿ translate ⎾language⏌ ⎾reply⏌
+⏐   ⦿ wiki ⎾text⏌
+⏐   ⦿ urlshort ⎾url⏌
+⏐   ⦿ pickup
+⏐   ⦿ landscape
+⏐   ⦿ landscape ⎾text⏌
+⏐   ⦿ portrait
+⏐   ⦿ portrait ⎾text⏌
+⏐   ⦿ mal ⎾text⏌
+⏐   ⦿ mdl ⎾text⏌
+⏐   ⦿ pin add
+⏐   ⦿ pin remove
+⏐   ⦿ sadcat ⎾text⏌
+⏐   ⦿ biden ⎾text⏌
+⏐   ⦿ pika ⎾text⏌
+⏐   ⦿ god ⎾text⏌
+⏐   ⦿ qrcode ⎾text⏌
+⏐
+⎩⎯  Some of my commands works in any means you could imagine.
 `;
 
 let help2 = `
-Hello %USER%, is there any questions you may have?
-
-   ⦿ verse today
-   ⦿ verse random
-   ⦿ verse [book] [chapter]: [verse]
-   ⦿ animeqoute
-   ⦿ bgremove
-   ⦿ motivate
-   ⦿ inspiration
-   ⦿ advice
-   ⦿ alert [text]
-   ⦿ meme
-   ⦿ lovetest [@name1]: [@name2]
-   ⦿ drake [text1]: [text2]
-   ⦿ pooh [text1]: [text2]
-   ⦿ oogway [text]
-   ⦿ caution [text]
-   ⦿ element [name]
-   ⦿ imdb [title]
-   ⦿ steam [name]
-   ⦿ npm [name]
-   ⦿ wfind [text]
-
-The : divides the query into 2 sections the first one and second one.
+⋆｡° Hello %USER%, is there any questions you may have?
+⏐
+⏐   ⦿ verse today
+⏐   ⦿ verse random
+⏐   ⦿ verse ⎾book⏌ ⎾chapter⏌: ⎾verse⏌
+⏐   ⦿ animeqoute
+⏐   ⦿ bgremove
+⏐   ⦿ motivate
+⏐   ⦿ inspiration
+⏐   ⦿ advice
+⏐   ⦿ alert ⎾text⏌
+⏐   ⦿ meme
+⏐   ⦿ lovetest ⎾@name1⏌: ⎾@name2⏌
+⏐   ⦿ drake ⎾text1⏌: ⎾text2⏌
+⏐   ⦿ pooh ⎾text1⏌: ⎾text2⏌
+⏐   ⦿ oogway ⎾text⏌
+⏐   ⦿ caution ⎾text⏌
+⏐   ⦿ element ⎾name⏌
+⏐   ⦿ imdb ⎾title⏌
+⏐   ⦿ steam ⎾name⏌
+⏐   ⦿ npm ⎾name⏌
+⏐   ⦿ wfind ⎾text⏌
+⏐
+⎩⎯  The : divides the query into 2 sections the first one and second one.
 `;
 
 let help3 = `
-Hello %USER%, how can i assist you further?
-
-   ⦿ mnm ^^
-   ⦿ trump ^^
-   ⦿ stalk ^^
-   ⦿ invert ^^
-   ⦿ greyscale ^^
-   ⦿ lick ^^
-   ⦿ hug ^^
-   ⦿ jokeover ^^
-   ⦿ cuddle ^^
-   ⦿ kiss ^^
-   ⦿ pet ^^
-   ⦿ jail ^^
-   ⦿ communist ^^
-   ⦿ wanted ^^
-   ⦿ gun ^^
-   ⦿ drip ^^
-   ⦿ clown ^^
-   ⦿ uncover ^^
-   ⦿ advert ^^
-   ⦿ blur ^^
-
-The ^^ means my command support mentioned, reply, uid etc.. to work.
+⋆｡° Hello %USER%, how can i assist you further?
+⏐
+⏐   ⦿ mnm ^^
+⏐   ⦿ trump ^^
+⏐   ⦿ stalk ^^
+⏐   ⦿ invert ^^
+⏐   ⦿ greyscale ^^
+⏐   ⦿ lick ^^
+⏐   ⦿ hug ^^
+⏐   ⦿ jokeover ^^
+⏐   ⦿ cuddle ^^
+⏐   ⦿ kiss ^^
+⏐   ⦿ pet ^^
+⏐   ⦿ jail ^^
+⏐   ⦿ communist ^^
+⏐   ⦿ wanted ^^
+⏐   ⦿ gun ^^
+⏐   ⦿ drip ^^
+⏐   ⦿ clown ^^
+⏐   ⦿ uncover ^^
+⏐   ⦿ advert ^^
+⏐   ⦿ blur ^^
+⏐
+⎩⎯  The ^^ means my command support mentioned, reply, uid etc.. to work.
 `;
 
 let help4 = `
-If you have any issue please do let me know.
-
-   ⦿ morse [text]
-   ⦿ joke
-   ⦿ profilepic
-   ⦿ wyr
-   ⦿ 8ball
-   ⦿ gmember
-   ⦿ car
-   ⦿ rcolor
-   ⦿ ship @mention @mention
-   ⦿ www @mention @mention
-   ⦿ trump [text]
-   ⦿ mock [text]
-   ⦿ reverseText [text]
-   ⦿ itunes [title]
-   ⦿ coding
-   ⦿ newyear
-   ⦿ christmas
-   ⦿ barrier
-   ⦿ fact
-   ⦿ thoughts
-
-Do you have any questions about the command %USER%?
+⋆｡° If you have any issue please do let me know.
+⏐
+⏐   ⦿ morse ⎾text⏌
+⏐   ⦿ joke
+⏐   ⦿ profilepic
+⏐   ⦿ wyr
+⏐   ⦿ 8ball
+⏐   ⦿ gmember
+⏐   ⦿ car
+⏐   ⦿ rcolor
+⏐   ⦿ ship @mention @mention
+⏐   ⦿ www @mention @mention
+⏐   ⦿ trump ⎾text⏌
+⏐   ⦿ mock ⎾text⏌
+⏐   ⦿ reverseText ⎾text⏌
+⏐   ⦿ itunes ⎾title⏌
+⏐   ⦿ coding
+⏐   ⦿ newyear
+⏐   ⦿ christmas
+⏐   ⦿ barrier
+⏐   ⦿ fact
+⏐   ⦿ thoughts
+⏐
+⎩⎯  Do you have any questions about the command %USER%?
 `;
 
 let help5 = `
-Here:
- 
-   ⦿ totext
-   ⦿ nba [name]
-   ⦿ doublestruck [text]
-   ⦿ count
-   ⦿ count --vowels
-   ⦿ count --consonants
-   ⦿ time
-   ⦿ time [timezone]
-   ⦿ anime [category]
-       waifu, neko, shinobu, megumin,
-       bully, cuddle, cry, hug,
-       awoo, kiss, lick, pat,
-       smug, bonk, yeet, blush,
-       smile, wave, highfive, handhold,
-       nom, bite, glomp, slap,
-       kill, kick, happy, wink,
-       poke, dance and cringe
-   ⦿ hanime [category]
-   ⦿ fbdl [url]
-   ⦿ ugly
-   ⦿ rugly
-   ⦿ ascii
-   ⦿ rascii
-
-
-How can i assist you further %USER%?
+⋆｡° Here:
+⏐
+⏐   ⦿ totext
+⏐   ⦿ nba ⎾name⏌
+⏐   ⦿ doublestruck ⎾text⏌
+⏐   ⦿ count
+⏐   ⦿ count --vowels
+⏐   ⦿ count --consonants
+⏐   ⦿ time
+⏐   ⦿ time ⎾timezone⏌
+⏐   ⦿ anime ⎾category⏌
+⏐       waifu, neko, shinobu, megumin,
+⏐       bully, cuddle, cry, hug,
+⏐       awoo, kiss, lick, pat,
+⏐       smug, bonk, yeet, blush,
+⏐       smile, wave, highfive, handhold,
+⏐       nom, bite, glomp, slap,
+⏐       kill, kick, happy, wink,
+⏐       poke, dance and cringe
+⏐   ⦿ hanime ⎾category⏌
+⏐   ⦿ fbdl ⎾url⏌
+⏐   ⦿ ugly
+⏐   ⦿ rugly
+⏐   ⦿ ascii
+⏐   ⦿ rascii
+⏐
+⏐
+⎩⎯  How can i assist you further %USER%?
 `;
 
 let help6 = `
-7th command list:
-
-   ⦿ encodeBinary [text]
-   ⦿ decodeBinary [text]
-   ⦿ sayjap [text]
-   ⦿ mathfacts
-   ⦿ ss [url]
-   ⦿ traceroute [url]
-   ⦿ nslookup [url]
-   ⦿ header [url]
-   ⦿ pair
-   ⦿ rpair
-   ⦿ checkPicture
-   ⦿ conan
-   ⦿ summ [text]
-   ⦿ gcolor [theme]
-       DefaultBlue, HotPink, AquaBlue, BrightPurple
-       CoralPink, Orange, Green, LavenderPurple
-       Red, Yellow, TealBlue, Aqua
-       Mango, Berry, Citrus, Candy
-
-Do you have anything in mind %USER%?
+⋆｡° 7th command list:
+⏐
+⏐   ⦿ encodeBinary ⎾text⏌
+⏐   ⦿ decodeBinary ⎾text⏌
+⏐   ⦿ sayjap ⎾text⏌
+⏐   ⦿ mathfacts
+⏐   ⦿ ss ⎾url⏌
+⏐   ⦿ traceroute ⎾url⏌
+⏐   ⦿ nslookup ⎾url⏌
+⏐   ⦿ header ⎾url⏌
+⏐   ⦿ pair
+⏐   ⦿ rpair
+⏐   ⦿ checkPicture
+⏐   ⦿ conan
+⏐   ⦿ summ ⎾text⏌
+⏐   ⦿ gcolor ⎾theme⏌
+⏐       DefaultBlue, HotPink, AquaBlue, BrightPurple
+⏐       CoralPink, Orange, Green, LavenderPurple
+⏐       Red, Yellow, TealBlue, Aqua
+⏐       Mango, Berry, Citrus, Candy
+⏐
+⎩⎯  Do you have anything in mind %USER%?
 `;
 
 let help7 = `
-Here is the 8th cmd list %USER%
-
-   ⦿ encrypt [text]
-   ⦿ decrypt [text] [key1]:[key2]
-   ⦿ facts [text]
-   ⦿ pat ^^
-   ⦿ blush ^^
-   ⦿ highfive ^^
-   ⦿ slap ^^
-   ⦿ bite ^^
-   ⦿ pat ^^
-   ⦿ encode64 [text]
-   ⦿ decode64 [text]
-   ⦿ tagalogSupport [on|off]
-   ⦿ textToSpeech [on|off]
-   ⦿ meowfacts
-   ⦿ dns4 [url]
-   ⦿ dns6 [url]
-   ⦿ musiclyric [title]
-   ⦿ videolyric [title]
-   ⦿ formatNumbers [numbers]
-   ⦿ fbi
-
-Wow, do you like me?
+⋆｡° Here is the 8th cmd list %USER%
+⏐
+⏐   ⦿ encrypt ⎾text⏌
+⏐   ⦿ decrypt ⎾text⏌ ⎾key1⏌:⎾key2⏌
+⏐   ⦿ facts ⎾text⏌
+⏐   ⦿ pat ^^
+⏐   ⦿ blush ^^
+⏐   ⦿ highfive ^^
+⏐   ⦿ slap ^^
+⏐   ⦿ bite ^^
+⏐   ⦿ pat ^^
+⏐   ⦿ encode64 ⎾text⏌
+⏐   ⦿ decode64 ⎾text⏌
+⏐   ⦿ tagalogSupport ⎾on⎨off⏌
+⏐   ⦿ textToSpeech ⎾on⎨off⏌
+⏐   ⦿ meowfacts
+⏐   ⦿ dns4 ⎾url⏌
+⏐   ⦿ dns6 ⎾url⏌
+⏐   ⦿ musiclyric ⎾title⏌
+⏐   ⦿ videolyric ⎾title⏌
+⏐   ⦿ formatNumbers ⎾numbers⏌
+⏐   ⦿ fbi
+⏐
+⎩⎯  Wow, do you like me?
 `;
 
 let help8 = `
-Hello %USER%, here is the last page of the command list.
-
-   ⦿ datefacts
-   ⦿ triviafacts
-   ⦿ yearfacts
-   ⦿ covid
-   ⦿ covid [country]
-   ⦿ parseImage [url]
-   ⦿ ping [url]
-   ⦿ dictionary [text]
-   ⦿ say [text]
-   ⦿ baybayin [text]
-   ⦿ weather [location]
-   ⦿ music [text]
-   ⦿ video [text]
-   ⦿ lyrics [text]
-   ⦿ github [username]
-   ⦿ kick ^^
-   ⦿ wink ^^
-   ⦿ poke ^^
-   ⦿ cringe ^^
-   ⦿ kill ^^
-   ⦿ smug ^^ 
-
-This is the end. Yes i know.
+⋆｡° Hello %USER%, here is the last page of the command list.
+⏐
+⏐   ⦿ datefacts
+⏐   ⦿ triviafacts
+⏐   ⦿ yearfacts
+⏐   ⦿ covid
+⏐   ⦿ covid ⎾country⏌
+⏐   ⦿ parseImage ⎾url⏌
+⏐   ⦿ ping ⎾url⏌
+⏐   ⦿ dictionary ⎾text⏌
+⏐   ⦿ say ⎾text⏌
+⏐   ⦿ baybayin ⎾text⏌
+⏐   ⦿ weather ⎾location⏌
+⏐   ⦿ music ⎾text⏌
+⏐   ⦿ video ⎾text⏌
+⏐   ⦿ lyrics ⎾text⏌
+⏐   ⦿ github ⎾username⏌
+⏐   ⦿ kick ^^
+⏐   ⦿ wink ^^
+⏐   ⦿ poke ^^
+⏐   ⦿ cringe ^^
+⏐   ⦿ kill ^^
+⏐   ⦿ smug ^^ 
+⏐
+⎩⎯  This is the end. Yes i know.
 `;
 
 let helpadmin = `
-Hello %USER%, this is the admin panel.
-
-   ⦿ unsend
-   ⦿ unsend [on|off]
-   ⦿ delay [on|off]
-   ⦿ nsfw [on|off]
-   ⦿ debug [on|off]
-   ⦿ antiLeave [on|off]
-   ⦿ simultaneousExecution [on|off]
-   ⦿ clearCache
-   ⦿ refreshState
-   ⦿ saveState
-   ⦿ fontIgnore ^^
-   ⦿ isBot ^^
-   ⦿ kickUser ^^
-   ⦿ blockUser ^^
-   ⦿ unblockUser ^^
-   ⦿ blockGroup
-   ⦿ unblockGroup
-   ⦿ setPrefix [prefix]
-   ⦿ remPrefix
-   ⦿ ignore [prefix]
-   ⦿ setKey [name]:[key]
-
-You guessed it, Only admins of the AI can used this commands.
+⋆｡° Hello %USER%, this is the admin panel.
+⏐
+⏐   ⦿ unsend
+⏐   ⦿ unsend ⎾on⎨off⏌
+⏐   ⦿ delay ⎾on⎨off⏌
+⏐   ⦿ nsfw ⎾on⎨off⏌
+⏐   ⦿ debug ⎾on⎨off⏌
+⏐   ⦿ antiLeave ⎾on⎨off⏌
+⏐   ⦿ simultaneousExecution ⎾on⎨off⏌
+⏐   ⦿ clearCache
+⏐   ⦿ refreshState
+⏐   ⦿ saveState
+⏐   ⦿ fontIgnore ^^
+⏐   ⦿ isBot ^^
+⏐   ⦿ kickUser ^^
+⏐   ⦿ blockUser ^^
+⏐   ⦿ unblockUser ^^
+⏐   ⦿ blockGroup
+⏐   ⦿ unblockGroup
+⏐   ⦿ setPrefix ⎾prefix⏌
+⏐   ⦿ remPrefix
+⏐   ⦿ ignore ⎾prefix⏌
+⏐   ⦿ setKey ⎾name⏌:⎾key⏌
+⏐
+⎩⎯  You guessed it, Only admins of the AI can used this commands.
 `;
 
 let helpuser = `
-Hello %USER%, this is the command list for user.
-
-   ⦿ addInstance
-   ⦿ setTimezone [timezone]
-   ⦿ uid 
-   ⦿ mute
-   ⦿ unmute
-   ⦿ smartReply [on|off]
-   ⦿ setNickname [text]
-   ⦿ acceptMessageRequest
-   ⦿ rname
-   ⦿ balance
-   ⦿ setBirthday [date]
-   ⦿ setGender [gender]
-   ⦿ setUsername [username]
-   ⦿ setAddress [address]
-   ⦿ setBio [info]
-
-You can set those information to enable awesome features.
+⋆｡° Hello %USER%, this is the command list for user.
+⏐
+⏐   ⦿ addInstance
+⏐   ⦿ setTimezone ⎾timezone⏌
+⏐   ⦿ uid 
+⏐   ⦿ mute
+⏐   ⦿ unmute
+⏐   ⦿ smartReply ⎾on⎨off⏌
+⏐   ⦿ setNickname ⎾text⏌
+⏐   ⦿ acceptMessageRequest
+⏐   ⦿ rname
+⏐   ⦿ balance
+⏐   ⦿ setBirthday ⎾date⏌
+⏐   ⦿ setGender ⎾gender⏌
+⏐   ⦿ setUsername ⎾username⏌
+⏐   ⦿ setAddress ⎾address⏌
+⏐   ⦿ setBio ⎾info⏌
+⏐
+⎩⎯  You can set those information to enable awesome features.
 `;
 
 let helpgroup = `
-Hello %USER%, this is the command list for groups.
-
-   ⦿ gname
-   ⦿ ginfo
-   ⦿ guid
-   ⦿ gphoto
-   ⦿ addUser [uid]
-   ⦿ everyone
-
-You can set those information to enable awesome features.
+⋆｡° Hello %USER%, this is the command list for groups.
+⏐
+⏐   ⦿ gname
+⏐   ⦿ ginfo
+⏐   ⦿ guid
+⏐   ⦿ gphoto
+⏐   ⦿ addUser ⎾uid⏌
+⏐   ⦿ everyone
+⏐ 
+⎩⎯  You can set those information to enable awesome features.
 `;
 
 let helproot = `
-Hello %USER%, you reached the root user command list.
-
-   ⦿ addCORS [url]
-   ⦿ remCORS [url]
-   ⦿ shell [code]
-   ⦿ git [code]
-   ⦿ sql [query]
-   ⦿ stop
-   ⦿ sync
-   ⦿ exit
-   ⦿ push
-   ⦿ resume
-   ⦿ restart
-   ⦿ notify
-   ⦿ destroy
-   ⦿ addAdmin ^^
-   ⦿ remAdmin ^^
-   ⦿ unblockAll
-   ⦿ unblockAll --bot
-   ⦿ acceptMessageRequest [threadid]
-   ⦿ acceptFriendRequest [uid]
-   ⦿ changeBio [text]
-   ⦿ angry [on|off]
-   ⦿ setMaxImage [integer]
-   ⦿ setTextComplextion [complextion]
-   ⦿ setMaxTokens [integer]
-   ⦿ setTemperature [integer]
-   ⦿ setFrequencyPenalty [integer]
-   ⦿ setProbabilityMass [integer]
-   ⦿ setAutoMarkRead [on|off]
-   ⦿ setOnline [on|off]
-   ⦿ setSelfListen [on|off]
-   ⦿ setSendTypingIndicator [on|off]
-   ⦿ setAutoMarkDelivery [on|off]
-   ⦿ setPresence [on|off]
-   ⦿ setReportingThread [uid]
-
-Only the AI Administrator can use this command.
+⋆｡° Hello %USER%, you reached the root user command list.
+⏐
+⏐   ⦿ addCORS ⎾url⏌
+⏐   ⦿ remCORS ⎾url⏌
+⏐   ⦿ shell ⎾code⏌
+⏐   ⦿ git ⎾code⏌
+⏐   ⦿ sql ⎾query⏌
+⏐   ⦿ stop
+⏐   ⦿ sync
+⏐   ⦿ exit
+⏐   ⦿ push
+⏐   ⦿ resume
+⏐   ⦿ restart
+⏐   ⦿ notify
+⏐   ⦿ destroy
+⏐   ⦿ addAdmin ^^
+⏐   ⦿ remAdmin ^^
+⏐   ⦿ unblockAll
+⏐   ⦿ unblockAll --bot
+⏐   ⦿ acceptMessageRequest ⎾threadid⏌
+⏐   ⦿ acceptFriendRequest ⎾uid⏌
+⏐   ⦿ changeBio ⎾text⏌
+⏐   ⦿ angry ⎾on⎨off⏌
+⏐   ⦿ setMaxImage ⎾integer⏌
+⏐   ⦿ setTextComplextion ⎾complextion⏌
+⏐   ⦿ setMaxTokens ⎾integer⏌
+⏐   ⦿ setTemperature ⎾integer⏌
+⏐   ⦿ setFrequencyPenalty ⎾integer⏌
+⏐   ⦿ setProbabilityMass ⎾integer⏌
+⏐   ⦿ setAutoMarkRead ⎾on⎨off⏌
+⏐   ⦿ setOnline ⎾on⎨off⏌
+⏐   ⦿ setSelfListen ⎾on⎨off⏌
+⏐   ⦿ setSendTypingIndicator ⎾on⎨off⏌
+⏐   ⦿ setAutoMarkDelivery ⎾on⎨off⏌
+⏐   ⦿ setPresence ⎾on⎨off⏌
+⏐   ⦿ setReportingThread ⎾uid⏌
+⏐
+⎩⎯  Only the AI Administrator can use this command.
 `;
 
 module.exports = {
