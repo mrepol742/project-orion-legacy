75320751b57013d492f35df4d3f08ac665552192
Melvin Jones Repol <mrepol742@gmail.com>
2023-10-15 22:58:05 +0800

    Initial Commit

--- src/test1.js
+++ src/test1.js
@@ -1,7 +1,4 @@
-const { execFile } = require('child_process');
-const child = execFile('node', ['--version'], (error, stdout, stderr) => {
-  if (error) {
-    throw error;
-  }
-  console.log(stdout);
-}); 
+const { exec } = require('child_process');
+exec("curl -I google.com", function (err, stdout, stderr) {
+   console.log(stdout)
+});
